b83d43531139d6e57cb03f220318e1af3f9c4bcc
koffi09 <45895139+koffi09@users.noreply.github.com>
2019-02-20 22:57:42 +0100

    Add files via upload

---
+++ entete1.php
@@ -0,0 +1,80 @@
+                 
+
+  
+  <!--ENTETE TU SITE  php -->>
+    
+            <?php echo '
+
+      <div class="container-fluid">
+    <div class="row">
+      <div class=" col-xs-12 col-md-4  col-sm-4">
+        <h1><a href="cours-renfo.php">CR3TL </a></h1>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3>
+          <b>Cours de renforcement<br> en  vidéo en ligne </b>
+        </h3>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 bg">
+        <h2>
+           &nbsp &nbsp  
+          <b>"Contact" <br>  +225 49249477 </b>
+             <p style="font-size:20px;color:#37BDBF">&nbsp &nbsp &nbsp &nbspAppel facturé </p>
+        </h2>
+      </div>
+    </div>
+  </div>
+  <nav class="my_navbar">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="cours-renfo.php"><span class="glyphicon glyphicon-home"> ACCEUIL</span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="active"><a href="#">FORUM</a></li>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">COURS<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="cours-3eme.php">NIVEAU 3ème</a></li>
+                  <li><a href="cours-terminalA.php">NIVEAU Tle A1-A2</a></li>
+                  <li><a href="cours-terminalc.php">NIVEAU TLE C-E</a></li>
+                  <li><a href="cours-terminald.php">NIVEAU TLE D</a></li>
+                </ul>
+              </li>
+              <li><a href="inscription.php">INSCRIPTION</a></li>
+              <li><a href="connexion.php">CONNECTION</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>   '?>
+
+  
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
